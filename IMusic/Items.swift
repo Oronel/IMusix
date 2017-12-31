4d6eacff8bd9d7e5fcc8c5ca22d5d0fa3406f90b
Lulu <orone.l@hotmail.fr>
2017-12-31 13:53:52 +0200

    added xcodeProject

---
+++ IMusic/Items.swift
@@ -0,0 +1,14 @@
+//
+//  Items.swift
+//  IMusic
+//
+//  Created by Mac on 31/12/2017.
+//  Copyright © 2017 Developers institute. All rights reserved.
+//
+
+import Foundation
+
+struct Items {
+    var artistName : String
+    var numberOfListeners : Int
+}
---
+++ IMusic/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  IMusic
+//
+//  Created by Mac on 31/12/2017.
+//  Copyright © 2017 Developers institute. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
